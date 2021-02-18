9455c19f216351dd3bb8141fccf326d16acc0c25
Somesh Mahendra <mahendrasomesh1491@gmail.com>
2021-02-18 19:09:28 +0530

    *Created PathFinder and PathFinderEditor Script.

---
+++ Editor/PathFinderEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ChaseAndRun
+{
+  [CustomEditor(typeof(PathFinder))]
+  public class PathFinderEditor : Editor
+  {
+    private PathFinder pathFinder;
+    private Mesh mesh;
+    private void OnEnable()
+    {
+      pathFinder = (PathFinder)target;
+      mesh = pathFinder.GetComponent<MeshFilter>().sharedMesh;
+    }
+
+    private void OnSceneGUI()
+    {
+      
+    }
+
+    private void DrawGrid()
+    {
+
+    }
+  }
+}
---
+++ PathFinder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChaseAndRun
+{
+  public class PathFinder : MonoBehaviour
+  {
+    public Vector2Int GridDimension { get { return gridDimension; } }
+ 
+    [SerializeField]
+    Vector2Int gridDimension;
+  }
+}
+
